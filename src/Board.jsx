418a79a999c31d61dc8216b042945d70ce09885a
tamuten <tamuten310@gmail.com>
2022-04-28 16:30:53 +0900

    #4 勝者判定までHooksで書き換え

--- src/Board.jsx
+++ src/Board.jsx
@@ -14,7 +14,7 @@ export const Board = (props) => {
 
   const handleClick = (i) => {
     const squaresSlice = squares.slice();
-    if (squaresSlice[i]) {
+    if (calculateWinner(squares) || squaresSlice[i]) {
       return;
     }
     squaresSlice[i] = xIsNext ? "X" : "O";
@@ -22,9 +22,17 @@ export const Board = (props) => {
     setSquares(squaresSlice);
   };
 
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
   return (
     <div>
-      <div className="status">Next player: {xIsNext ? "X" : "O"}</div>
+      <div className="status">{status}</div>
       <div className="board-row">
         {renderSquare(0)}
         {renderSquare(1)}
@@ -42,4 +50,24 @@ export const Board = (props) => {
       </div>
     </div>
   );
+};
+
+const calculateWinner = (squares) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
